6e2381ff73e8cc72a57b257926fc86e6fa15defa
Kyomuhendo Milliam <milliamkyomuhendo@gmail.com>
2025-03-08 18:10:49 +0300

    updating the sub-contents

---
+++ ML-AI-projects/Deep_Learning/CNN_CFAR10/README.md
@@ -0,0 +1,4 @@
+# Convolution Neural Network Task 2
+Design a CNN for a classification task (using anyone of the deep learning frameworks, e.g., Pytorch, Keras and tensor flow) with CIFAR-10.
+# Instructions 
+Evaluate the performance of your model in terms of training and testing accuracy, and training and test error.
---
+++ Python_Programming/informal_intro.py
@@ -0,0 +1,84 @@
+Python 3.11.9 (tags/v3.11.9:de54cf5, Apr  2 2024, 10:12:12) [MSC v.1938 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+# This is the first comment
+spam = 1 # Declaring spam
+text = "# This is not a comment because it is inside quotes"
+
+print(spam)
+1
+
+print(text)
+# This is not a comment because it is inside quotes
+
+#Using the python Calculator
+
+# Adding numbers
+2 + 2
+4
+
+# Subtracting, Multiplying and Dividing numbers
+((50) - (5 * 6)) / 4
+5.0
+
+
+# Dividing numbers
+8 / 5
+1.6
+
+# Eliminating the decimal point
+8 // 5
+1
+>>> 
+>>> # Remainder values in division
+>>> 8 % 5
+3
+>>> 
+>>> # The power operator
+>>> 4**2
+16
+>>> 
+>>> # Assigning values to variables
+>>> width = 20
+>>> height = 5 * 9
+>>> 
+>>> 
+>>> width - height
+-25
+>>> 
+>>> # trying to access an un assigned variable
+>>> m
+Traceback (most recent call last):
+  File "<pyshell#37>", line 1, in <module>
+    m
+NameError: name 'm' is not defined
+>>>  m = "name"
+...  
+SyntaxError: unexpected indent
+>>> m = "name"
+>>> print(m)
+name
+>>> name
+Traceback (most recent call last):
+  File "<pyshell#41>", line 1, in <module>
+    name
+NameError: name 'name' is not defined
+>>> 
+>>> 
+>>> tax = 12.5 / 100
+>>> price = 100.50
+>>> price * tax
+12.5625
+price + _
+113.0625
+round(_, 2)
+113.06
+round(_, 1)
+113.1
+
+# single quotes and double quotes
+"don't"
+"don't"
+'don't'
+SyntaxError: unterminated string literal (detected at line 1)
+'don\'t'
+"don't"
